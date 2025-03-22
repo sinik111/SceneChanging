dbd5625c33072599ff18e12acb6b2eb6f5e6fc52
sinik123 <kangmark95@naver.com>
2025-03-22 21:51:41 +0900

    feature debug log, debug leak check

--- src/Global.c
+++ src/Global.c
@@ -1,5 +1,9 @@
 #include "Global.h"
 
+#include <windows.h>
+#include <stdio.h>
+#include <stdarg.h>
+
 int g_quit = 0;
 
 // 다른 파일에서 직접 접근하지 못하게 static으로 선언
@@ -9,3 +13,20 @@ long long GenerateID()
 {
 	return g_global_id++;
 }
+
+#ifdef _DEBUG
+void PrintDebugLog(const char* fmt, ...)
+{
+    char buffer[1024];
+
+    va_list args;
+
+    va_start(args, fmt);
+
+    vsnprintf_s(buffer, sizeof(buffer), _TRUNCATE, fmt, args);
+
+    va_end(args);
+
+    OutputDebugStringA(buffer);
+}
+#endif
--- src/main.c
+++ src/main.c
@@ -1,24 +1,11 @@
-#ifdef _DEBUG
-#define MEMORY_LEAK_CHECK // 메모리 누수 체크. 해당 줄 주석 처리하면 체크 안함.
-
-#ifdef MEMORY_LEAK_CHECK
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-
-#endif // MEMORY_LEAK_CHECK
-#endif // _DEBUG
-
-#include <stdlib.h>
-
+#include "MemoryLeakCheck.h"
 #include "Game.h"
 #include "MyTime.h"
 #include "Global.h"
 
 int main()
 {
-#ifdef MEMORY_LEAK_CHECK
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
+	ENABLE_LEAK_CHECK();
 
 	InitializeTime(); // 시간 초기화.
 	UpdateTime(); // 처음에 한번 UpdateTime 해줌.
@@ -34,7 +21,5 @@ int main()
 
 	ReleaseGame(); // 동적할당 해제 등 처리.
 
-#ifdef MEMORY_LEAK_CHECK
-	_CrtDumpMemoryLeaks();
-#endif
+	DUMP_LEAKS();
 }
