7a3eca09cd42d45a6b28a4a74e1b85ceacc2e702
sinik111 <kangmark95@naver.com>
2025-03-24 08:50:29 +0900

    Input.h 업데이트

--- inc/Input.h
+++ inc/Input.h
@@ -2,6 +2,11 @@
 
 #include "Types.h"
 
+#define VK_LEFT           0x25
+#define VK_UP             0x26
+#define VK_RIGHT          0x27
+#define VK_DOWN           0x28
+
 void UpdateInput();
 
 BOOL IsKeyDown(int vKey);
